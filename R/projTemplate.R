ce3581917b8e030636a34e89dadeb12ab3a01d41
markwh <mark.hagemann@gmail.com>
2015-08-25 10:34:18 -0400

    Added function to read from ProjectTemplate cache. Added documentation.

---
+++ R/projTemplate.R
@@ -0,0 +1,15 @@
+# Functions for project organization, assuming use of ProjectTemplate package
+
+#' Fetch a single object from cache
+#' 
+#' Does the opposite of ProjectTemplate::cache -- given an object name (as a
+#' character string), retrieves that object from the cache directory.
+#' 
+#' @param variable character, the name of the variable to be retrieved.
+#' @param value None, used for side-effects only
+#' @seealso ProjectTemplate::cache
+
+
+uncache <- function(variable, envir = parent.frame()) {
+  load(file.path("cache", paste0(variable, ".RData")), envir = envir)
+}
---
+++ tests/testthat/test-projTemplate.R
@@ -0,0 +1,26 @@
+context("projectTemplate functions")
+
+test_that("uncache works for different environments", {
+  isthere <- dir.exists("cache")
+  if(!isthere) {
+    dir.create("cache")
+    on.exit(file.remove("cache"))
+  }
+  
+  # put object in cache
+  foo = "abc"
+  save("foo", file = "cache/foo.RData")
+  rm(foo)
+  
+  # test
+#   browser()
+  expect_false(exists("foo", envir = parent.frame()))
+  uncache("foo")
+  expect_true(exists("foo", envir = parent.frame()))
+  rm(foo)
+  
+  # delete file
+  file.remove(file.path("cache", paste0("foo", ".RData")))
+  if(!isthere) 
+    file.remove("cache")
+})
