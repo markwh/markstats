e86be359bd5e549e014d31eca821ef46b2170c60
markhagemann <mark.hagemann@gmail.com>
2015-10-26 14:44:10 -0400

    Moved crossvalidate from rcmodel; fixed dumb bug in ocv function; added tests and documentation

--- R/crossvalidate.R
+++ R/crossvalidate.R
@@ -19,7 +19,7 @@ crossvalidate <- function(object, ...) {
 #' @export
 #' 
 ocv <- function(object) {
-  hat <- stats::hatvalues(goodlm)
+  hat <- stats::hatvalues(object)
   denom <- (1 - hat)^2
   ocv <- mean(residuals(object)^2 / denom)
   ocv
@@ -90,6 +90,99 @@ crossvalidate.lm <- function(object, kfolds = 0, statistic = c("R2", "mse", "mae
   
   sagg
 }
+
+
+
+#' Crossvalidation for rcgam models
+#'
+#' @param what what quantity to predict/validate
+#' @param retransform compare predictions to observations in original uints?
+#' Must be TRUE if what == "load_kg.d"
+#' @param ... Passed to predict.rcgam
+#'
+#' @export
+
+crossvalidate.rcgam  <- function(object, kfolds = 0,
+                                 statistic = c("R2", "mse", "mae", "rmse"),
+                                 what = c("conc_mg.l", "load_kg.d"),
+                                 retransform = TRUE, ...) {
+  
+  if (!requireNamespace("rcmodel", quietly = TRUE)) {
+    stop("rcmodel needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  
+  what = match.arg(what)
+  if(what == "load_kg.d" && ! retransform)
+    stop("Loads must be crossvalidated in original units")
+  data <- getData(object)
+  data$c <- object$transform$ctrans(data$conc)
+  data$load = rcmodel::calcLoad(data$conc, data$flow)
+  fmla <- object$formula
+  
+  yname <- ifelse(what == "conc_mg.l", ifelse(retransform, "conc", "c"),
+                  "load")
+  statistic = match.arg(statistic)
+  sfun = ifelse(statistic == "mae", "mae", "sse")
+  
+  if(kfolds == 0) {
+    preds = rep(NA_real_, nrow(data))
+    for(fold in 1:nrow(data)) {
+      train <- data[-fold, ]
+      test <- data[fold, ]
+      # curobj <- do.call("rcgam", list(formula = fmla, data = train))
+      curobj <- rcmodel::rcgam(formula = fmla, data = train)
+      
+      ypred <- as.numeric(predict(curobj, newdata = test, what = what,
+                                  retransform = retransform, ...)$fit)
+      ymeas <- test[[yname]]
+      preds[fold] = ypred
+    }
+    stat1 = do.call(sfun, list(data[[yname]], preds))
+    
+    if(statistic == "mae")
+      sagg = stat1
+    else if (statistic == "mse")
+      sagg = stat1 /  nrow(data)
+    else if (statistic == "rmse")
+      sagg = sqrt(stat1 / nrow(data))
+    else
+      sagg = 1 - stat1 / sum((data[[yname]] - mean(data[[yname]], na.rm = TRUE))^2)
+  } else {
+    # split data into folds
+    case.folds = sample(rep(1:kfolds, length.out = nrow(data)))
+    # browser()
+    fold.stat = rep(NA_real_, kfolds) # error statistic for each fold
+    
+    for (fold in 1:kfolds) {
+      train <- data[case.folds != fold, ]
+      test <- data[case.folds == fold, ]
+      curobj <- rcmodel::rcgam(formula = fmla, data = train)
+      
+      ypred <- as.numeric(predict(curobj, newdata = test, what = what,
+                                  retransform = retransform, ...)$fit)
+      ymeas <- test[[yname]]
+      fold.stat[fold] <- do.call(sfun, list(ymeas, ypred))
+    }
+    
+    # assemble folds
+    # r2 <- 1 - sum(fold.ss) / sum((ymeas - mean(ymeas, na.rm = TRUE))^2)
+    ymeas <- data[[yname]]
+    ssum <- sum(fold.stat)
+    
+    if(statistic == "mae")
+      sagg = ssum / kfolds
+    else if (statistic == "mse")
+      sagg = ssum / nrow(data)
+    else if (statistic == "rmse")
+      sagg = sqrt(ssum / nrow(data))
+    else
+      sagg = 1 - ssum / sum((ymeas - mean(ymeas, na.rm = TRUE))^2)
+  }
+  sagg
+}
+
+
 
 
 getMaker <- function(object) {
--- tests/testthat/test-crossval.R
+++ tests/testthat/test-crossval.R
@@ -11,9 +11,65 @@ test_that("crossvalidation returns reasonable numbers", {
 })
 
 test_that("lm LOO crossvalidation agrees with press statistic", {
-  data(goodlm)
-  fakedata = goodlm$model
-  goodlm <- eval(goodlm$call)
-  expect_equal(crossvalidate(goodlm, statistic = "mse"), qpcR::PRESS(goodlm)$stat / nrow(goodlm$model))
-  expect_equal(crossvalidate(goodlm), qpcR::PRESS(goodlm)$P.square)
+  data(mtcars)
+  
+  lm1 <- lm(mpg ~ disp, mtcars)
+  
+  expect_equal(crossvalidate(lm1, statistic = "mse"), qpcR::PRESS(lm1)$stat / nrow(lm1$model))
+  expect_equal(crossvalidate(lm1), qpcR::PRESS(lm1)$P.square)
+})
+
+
+## crossvalidate.rcgam
+
+test_that("crossvalidation works for rcgams", {
+  
+  data(Phosphorus, package = "rcmodel")
+  foo = rcmodel::makeModelData(Phosphorus)
+  mod2 = rcmodel::rcgam(c ~ s(q) + s(doy, bs = "cc", k = 4) + s(time), foo)
+  
+  conccvs = c(
+    concloo = crossvalidate(mod2, what = "conc"),
+    conc5f = crossvalidate(mod2, what = "conc", kf = 5),
+    concloo_nosmear = crossvalidate(mod2, what = "conc", smear = FALSE),
+    conc5f_nosmear = crossvalidate(mod2, what = "conc", smear = FALSE, kf = 5)
+  )
+  
+  loadcvs = c(
+    loadloo = crossvalidate(mod2, what = "load"),
+    load5f = crossvalidate(mod2, what = "load", kf = 5),
+    loadloo_nosmear = crossvalidate(mod2, what = "load", smear = FALSE),
+    load5f_nosmear = crossvalidate(mod2, what = "load", smear = FALSE, kf = 5)
+  )
+  
+  expect_is(conccvs, "numeric")
+  expect_is(crossvalidate(mod2, kfolds = 5), "numeric")
+  
+  expect_true(all(conccvs < 1))
+  expect_true(all(loadcvs < 1))
+  
+})
+
+test_that("crossvalidation works for log-space estimates in rcgams", {
+  data(Phosphorus, package = "rcmodel")
+  foo = rcmodel::makeModelData(Phosphorus)
+  mod2 = rcmodel::rcgam(c ~ s(q) + s(doy, bs = "cc", k = 4) + s(time), foo)
+  
+  concloo = crossvalidate(mod2, what = "conc", retransform = FALSE, smear = FALSE)
+  
+  conc5f = crossvalidate(mod2, what = "conc", retransform = FALSE, kf = 5)
+  concloo_nosmear = crossvalidate(mod2, what = "conc", retransform = FALSE,
+                                  smear = FALSE)
+  
+  expect_is(concloo, "numeric")
+  expect_is(conc5f, "numeric")
+  
+  expect_less_than(concloo, 1)
+  expect_less_than(conc5f, 1)
+  expect_equal(concloo, concloo_nosmear)
+  
+  expect_error(crossvalidate(mod2, what = "load", retransform = FALSE))
+  
+  #   mod2$gcv.ubre
+  #   crossvalidate(mod2, retransform = FALSE, statistic = "mse")
 })
