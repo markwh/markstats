add5e77ef3eb746617fb58a7e9537f1b9bf28d06
markhagemann <mark.hagemann@gmail.com>
2015-10-26 12:54:56 -0400

    exported, documented, tested goodness-of-fit stats

--- R/gofStats.R
+++ R/gofStats.R
@@ -1,14 +1,29 @@
 
+#' coefficient of determination
+#' @param obs observed data
+#' @param pred predictions
+#' 
+#' @export
 R2 <- function(obs, pred) {
   stopifnot(length(obs) == length(pred))
   1 - var(obs - pred) / var(obs)
 }
 
+#' mean absolute error
+#' @param obs observed data
+#' @param pred predictions
+#' 
+#' @export
 mae <- function(obs, pred) {
   stopifnot(length(obs) == length(pred))
   mean(abs(obs - pred))
 }
 
+#' sum of squared errors
+#' @param obs observed data
+#' @param pred predictions
+#' 
+#' @export
 sse <- function(obs, pred) {
   stopifnot(length(obs) == length(pred))
   sum((obs - pred)^2)
---
+++ tests/testthat/test-gof.R
@@ -0,0 +1,11 @@
+context("goodness-of-fit")
+
+
+test_that("coefficient of determination works", {
+  
+  xy <- data.frame(x = 1:10, y = rnorm(10) + 1:10)
+  yhat <- lm(y~x, xy)$fitted
+  
+  expect_equal(R2(xy$y, yhat), with(xy, cor(x, y)^2))
+  expect_equal(R2(xy$y, yhat), 1 - sum((xy$y - yhat)^2) / sum((xy$y - mean(xy$y))^2))
+})
