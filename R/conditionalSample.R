e245910da082286a04938685657084f1fc11668f
markhagemann <mark.hagemann@gmail.com>
2015-11-30 14:28:40 -0500

    getData is now back; all tests passed

--- R/conditionalSample.R
+++ R/conditionalSample.R
@@ -87,9 +87,10 @@ condlSample.lm <- function(object, newdata, quantile = "random", ...) {
 #' @param retransform Should the predictions be returned as concentrations? (defaults to TRUE)
 #' @param ... Arguments passed to `predict.lm` or predict.gam function call
 #' @param smear Use Smearing estimator to correct transformation bias?
+#' @describeIn condlSample Method for rcgam objects
 #' @importFrom dplyr "%>%" mutate_
 #' @export
-condlSample.rcmodel <- function(object, newdata, flowcol = "flow",
+condlSample.rcgam <- function(object, newdata, flowcol = "flow",
                               flow.units = "CFS", quantile, ...) {
   
   if (!requireNamespace("rcmodel", quietly = TRUE)) {
@@ -119,21 +120,43 @@ condlSample.rcmodel <- function(object, newdata, flowcol = "flow",
 }
 
 
-#' @describeIn condlSample Method for rcgam objects
-#' @export
-condlSample.rcgam <- function(object, newdata, flowcol = "flow",
-                              flow.units = "CFS", quantile, ...) {
-  out = condlSample.rcmodel(object = object, newdata = newdata, flowcol = flowcol,
-                            flow.units = flow.units, quantil = quantile, ...)
-  out
-}
-
-
+#' Predict method for rclm fits
+#'
+#' Predict values using an rclm or rcgam model object
+#'
+#' @param object An rclm or rcgam object to use for predicting
+#' @param newdata a data.frame containing precictor variables to use for prediction
+#' @param retransform Should the predictions be returned as concentrations? (defaults to TRUE)
+#' @param ... Arguments passed to `predict.lm` or predict.gam function call
+#' @param smear Use Smearing estimator to correct transformation bias?
 #' @describeIn condlSample Method for rclm objects
+#' @importFrom dplyr "%>%" mutate_
 #' @export
 condlSample.rclm <- function(object, newdata, flowcol = "flow",
-                             flow.units = "CFS", quantile, ...) {
-  out = condlSample.rcmodel(object = object, newdata = newdata, flowcol = flowcol,
-                            flow.units = flow.units, quantil = quantile, ...)
-  out
+                              flow.units = "CFS", quantile, ...) {
+  
+  if (!requireNamespace("rcmodel", quietly = TRUE)) {
+    stop("rcmodel needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  # library("mgcv")
+  if (missing(newdata))
+    newdata = getData(object)
+  
+  assertthat::assert_that(is(newdata$Date, "Date"))
+  assertthat::assert_that(flowcol %in% names(newdata))
+  assertthat::assert_that("flow.units" %in% names(newdata))
+  assertthat::assert_that(all(as.character(newdata$flow.units) == object$units["qunits"]))
+  
+  newdata <- newdata %>%
+    mutate_(q = ~ object$transform$qtrans(newdata[[flowcol]]),
+            time = ~ as.numeric(Date) - as.numeric(object$stats["datebar"]),
+            doy = ~ as.numeric(format(Date, "%j")))
+  preds = NextMethod("condlSample", object = object, newdata = newdata,
+                     quantile = quantile, smear = FALSE, retransform = FALSE, 
+                     ...)
+  #   preds = condlSample.lm(object = object, newdata = newdata,
+  #                          quantile = quantile, smear = FALSE, retransform = FALSE)
+  preds = object$transform$cinvert(preds)
+  preds
 }
