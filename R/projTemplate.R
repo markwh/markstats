ba3a6b19dc35ab7ecd1aa50da34ea1b1cd4b2765
markhagemann <mark.hagemann@gmail.com>
2016-05-11 13:48:25 -0400

    fromcache now accepts multiple arguments in the style of rm()

--- R/projTemplate.R
+++ R/projTemplate.R
@@ -5,12 +5,22 @@
 #' Does the opposite of ProjectTemplate::cache -- given an object name (as a
 #' character string), retrieves that object from the cache directory.
 #' 
-#' @param variable character, the name of the variable to be retrieved.
-#' @param value None, used for side-effects only
-#' @seealso ProjectTemplate::cache
+#' @param ...	the objects to be retrieved, as names (unquoted) or character strings (quoted).
+#' @param list	a character vector naming objects to be retrieved.
+#' @param envir the environment into which to retrieve the object.
+#' @return None, used for side-effects only
+#' @seealso  ProjectTemplate::cache
 #' @export
 
-
-fromcache <- function(variable, envir = parent.frame()) {
-  load(file.path("cache", paste0(variable, ".RData")), envir = envir)
+fromcache <- function(..., list = character(), envir = parent.frame()) {
+  dots <- match.call(expand.dots = FALSE)$...
+  if (length(dots) && !all(vapply(dots, function(x) is.symbol(x) || 
+                                  is.character(x), NA, USE.NAMES = FALSE))) 
+    stop("... must contain names or character strings")
+  names <- vapply(dots, as.character, "")
+  if (length(names) == 0L) 
+    names <- character()
+  list <- .Primitive("c")(list, names)
+  files <- file.path("cache", paste0(list, ".RData"))
+  lapply(files, load, envir = envir)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,20 +1,5 @@
 # Various utility functions
 
-#' Fetch a single object from cache
-#' 
-#' Does the opposite of ProjectTemplate::cache -- given an object name (as a
-#' character string), retrieves that object from the cache directory.
-#' 
-#' @param variable character, the name of the variable to be retrieved.
-#' @param value None, used for side-effects only
-#' @seealso ProjectTemplate::cache
-#' @export
-
-
-fromcache <- function(variable, envir = parent.frame()) {
-  load(file.path("cache", paste0(variable, ".RData")), envir = envir)
-}
-
 
 #' Get all of the pieces for a particular stringsplit.
 #' @param strvec Vector of strings, each to be split.
